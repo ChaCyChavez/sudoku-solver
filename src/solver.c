c349e68f2c79bcfa16e2489ef7964886bb7d7352
chacychavez <cschavez@up.edu.com>
2018-04-24 12:51:25 +0800

    edit convention

--- src/solver.c
+++ src/solver.c
@@ -2,26 +2,26 @@
 #include <stdlib.h>
 
 
-void printPuzzle(FILE *file_out, int ** puzzle, int puzzle_size){
+void print_puzzle(FILE *output_file, int ** puzzle, int puzzle_size) {
   // Print puzzle
   int i, j;
   for(i = 0; i < puzzle_size; i++) {
     for(j = 0; j < puzzle_size; j++) {
       printf("%d\t", puzzle[i][j]);
-      fprintf(file_out, "%d\t", puzzle[i][j]);
+      fprintf(output_file, "%d\t", puzzle[i][j]);
     }
     printf("\n");
-    fprintf(file_out, "\n");
+    fprintf(output_file, "\n");
   }
   printf("\n");
-  fprintf(file_out, "\n");
+  fprintf(output_file, "\n");
 }
 
-int solved(int ** puzzle, int puzzle_size) {
+int is_solved(int **puzzle, int puzzle_size) {
   int i, j;
   for(i = 0; i < puzzle_size; i++) {
     for(j = 0; j < puzzle_size; j++) {
-      if(puzzle[i][j] == 0){
+      if(puzzle[i][j] == 0) {
         return 0;
       }
     }
@@ -30,11 +30,11 @@ int solved(int ** puzzle, int puzzle_size) {
   return 1;
 }
 
-void findNext(int ** puzzle, int puzzle_size, int *row, int *col){
+void find_next(int **puzzle, int puzzle_size, int *row, int *col) {
   int i, j;
   for(i = 0; i < puzzle_size; i++) {
     for(j = 0; j < puzzle_size; j++) {  	
-      if(puzzle[i][j] == 0){
+      if(puzzle[i][j] == 0) {
         (*row) = i;
         (*col) = j;
         return;
@@ -43,59 +43,59 @@ void findNext(int ** puzzle, int puzzle_size, int *row, int *col){
   }  
 }
 
-int hasConflict(int ** puzzle, int subgrid_size, int puzzle_size, int currRow, int currCol){
+int has_conflict(int **puzzle, int subgrid_size, int puzzle_size, int currRow, int currCol) {
   int i, j, rowMin, rowMax, colMin, colMax;
 
   //check row
-  for(i=0; i<puzzle_size; i++){
-    if(currCol != i && puzzle[currRow][i] == puzzle[currRow][currCol]){
+  for(i = 0; i < puzzle_size; i++) {
+    if(currCol != i && puzzle[currRow][i] == puzzle[currRow][currCol]) {
       return 1;
     }
   }
 
   //check column
-  for(i=0; i<puzzle_size; i++){
-    if(currRow != i && puzzle[i][currCol] == puzzle[currRow][currCol]){
+  for(i = 0; i < puzzle_size; i++) {
+    if(currRow != i && puzzle[i][currCol] == puzzle[currRow][currCol]) {
       return 1;
     }
   }
 
   //check box or quadrant
-  if((currRow + 1) == 1){
+  if((currRow + 1) == 1) {
   	rowMin = 0;
   	rowMax = subgrid_size;
-  }else if((currRow + 1)%subgrid_size == 0){
+  } else if((currRow + 1) % subgrid_size == 0) {
   	rowMin = (currRow + 1) - subgrid_size;
   	rowMax = currRow + 1;
-  }else{
+  } else {
   	rowMax = currRow + 1;
-  	while(1){
+  	while(1) {
   		rowMax++;
-  		if(rowMax%subgrid_size == 0) break;
+  		if(rowMax % subgrid_size == 0) break;
   	}
   	rowMin = rowMax - subgrid_size;
   }
 
-  if((currCol + 1) == 1){
+  if((currCol + 1) == 1) {
   	colMin = 0;
   	colMax = subgrid_size;
-  }else if((currCol + 1)%subgrid_size == 0){
+  } else if((currCol + 1) % subgrid_size == 0) {
   	colMin = (currCol + 1) - subgrid_size;
   	colMax = currCol + 1;
-  }else{
+  } else {
   	colMax = currCol + 1;
-  	while(1){
+  	while(1) {
   		colMax++;
-  		if(colMax%subgrid_size == 0) break;
+  		if(colMax % subgrid_size == 0) break;
   	}
   	colMin = colMax - subgrid_size;
   }  
 
 
-	for(i=rowMin; i<rowMax; i++){
-		for(j=colMin; j<colMax; j++){
+	for(i = rowMin; i < rowMax; i++) {
+		for(j = colMin; j < colMax; j++) {
 	    if((currRow != i && currCol != j) && 
-	    	puzzle[i][j] == puzzle[currRow][currCol]){
+	    	puzzle[i][j] == puzzle[currRow][currCol]) {
 	      return 1;
 	    }
 		}
@@ -103,26 +103,26 @@ int hasConflict(int ** puzzle, int subgrid_size, int puzzle_size, int currRow, i
   return 0;
 }
 
-int solvePuzzle(FILE *file_out, int ** puzzle, int subgrid_size, int puzzle_size, int *row, int *col, int *num_of_sols){
+int solve_puzzle(FILE *output_file, int ** puzzle, int subgrid_size, int puzzle_size, int *row, int *col, int *num_of_sols) {
   int i, j, x;
   int currRow = *row;
   int currCol = *col;
-  if(solved(puzzle, puzzle_size) == 1){
+  if(is_solved(puzzle, puzzle_size) == 1) {
   	(*num_of_sols)++;
     printf("Solution #%d:\n", (*num_of_sols));
-    fprintf(file_out, "Solution #%d:\n", (*num_of_sols));
-  	printPuzzle(file_out, puzzle, puzzle_size);
+    fprintf(output_file, "Solution #%d:\n", (*num_of_sols));
+  	print_puzzle(output_file, puzzle, puzzle_size);
     return 1;
   }
 
-  for(i=1; i<puzzle_size+1; i++){
+  for(i = 1; i < puzzle_size + 1; i++) {
     puzzle[currRow][currCol] = i;
-    if(hasConflict(puzzle, subgrid_size, puzzle_size, currRow, currCol) == 0){
-      findNext(puzzle, puzzle_size, row, col);
-      // if(solvePuzzle(puzzle, subgrid_size, puzzle_size, row, col) == 1){
+    if(has_conflict(puzzle, subgrid_size, puzzle_size, currRow, currCol) == 0) {
+      find_next(puzzle, puzzle_size, row, col);
+      // if(solve_puzzle(output_file, puzzle, subgrid_size, puzzle_size, row, col, num_of_sols) == 1) {
       //   return 1;
       // }
-      solvePuzzle(file_out, puzzle, subgrid_size, puzzle_size, row, col, num_of_sols);
+      solve_puzzle(output_file, puzzle, subgrid_size, puzzle_size, row, col, num_of_sols);
     }
   }
   puzzle[currRow][currCol] = 0; 
@@ -137,13 +137,13 @@ int main(void) {
   int row, col;
   int num_of_sols=0;
 
-  FILE *file = fopen("./input/in.txt", "r");
-  FILE *file_out = fopen("./output/out.txt", "w");
+  FILE *input_file = fopen("./input/in.txt", "r");
+  FILE *output_file = fopen("./output/out.txt", "w");
 
-  fscanf(file, "%d", &num_of_puzzles);
+  fscanf(input_file, "%d", &num_of_puzzles);
 
   while(num_of_puzzles--) {
-    fscanf(file, "%d", &subgrid_size);
+    fscanf(input_file, "%d", &subgrid_size);
     puzzle_size = subgrid_size * subgrid_size;
 
     // Initialize puzzle
@@ -156,17 +156,19 @@ int main(void) {
     // Fill puzzle 
     for(i = 0; i < puzzle_size; i++) {
       for(j = 0; j < puzzle_size; j++) {
-        fscanf(file, "%d", &puzzle[i][j]);
+        fscanf(input_file, "%d", &puzzle[i][j]);
       }
     }
 
-    fprintf(file_out, "Input:\n");
-    printPuzzle(file_out, puzzle, puzzle_size);
-    findNext(puzzle, puzzle_size, &row, &col);
-    solvePuzzle(file_out ,puzzle, subgrid_size, puzzle_size, &row, &col, &num_of_sols);
+    fprintf(output_file, "Input:\n");
+    print_puzzle(output_file, puzzle, puzzle_size);
+    
+    find_next(puzzle, puzzle_size, &row, &col);
+    solve_puzzle(output_file ,puzzle, subgrid_size, puzzle_size, &row, &col, &num_of_sols);
+    
     printf("Number of solutions: %d\n\n", num_of_sols);
-    fprintf(file_out, "Number of solutions: %d\n\n", num_of_sols);
-    // printPuzzle(puzzle, puzzle_size);
+    fprintf(output_file, "Number of solutions: %d\n\n", num_of_sols);
+
     // Free puzzle
     for(i = 0; i < subgrid_size; i++) {
       free(puzzle[i]);
@@ -176,8 +178,8 @@ int main(void) {
     num_of_sols = 0;
   }
 
-  fclose(file);
-  fclose(file_out);
+  fclose(input_file);
+  fclose(output_file);
 
   return 0;
 }
